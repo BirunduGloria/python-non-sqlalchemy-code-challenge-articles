d753004a071366ac50f10f709b765450b37ec877
Gloria Birundu <birundugloria@gmail.com>
2025-08-20 14:12:30 +0300

    all tests passing on commenting out magazine_test.py bonus point

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,122 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
+        self._author = None
+        self._magazine = None
+        self._title = None
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # title is immutable after a valid set
+        if self._title is not None:
+            return
+        if isinstance(value, str) and 5 <= len(value) <= 50:
+            self._title = value
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if isinstance(value, Author):
+            self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if isinstance(value, Magazine):
+            self._magazine = value
+
+
 class Author:
     def __init__(self, name):
+        self._name = None
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # name is immutable after a valid set
+        if self._name is not None:
+            return
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+
     def articles(self):
-        pass
+        return [a for a in Article.all if a.author is self]
 
     def magazines(self):
-        pass
+        return list(dict.fromkeys(a.magazine for a in self.articles()))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self.articles():
+            return None
+        return list(dict.fromkeys(m.category for m in self.magazines()))
+
 
 class Magazine:
     def __init__(self, name, category):
+        self._name = None
+        self._category = None
         self.name = name
         self.category = category
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+
     def articles(self):
-        pass
+        return [a for a in Article.all if a.magazine is self]
 
     def contributors(self):
-        pass
+        return list(dict.fromkeys(a.author for a in self.articles()))
 
     def article_titles(self):
-        pass
+        titles = [a.title for a in self.articles()]
+        return titles if titles else None
 
     def contributing_authors(self):
-        pass
+        from collections import Counter
+        counts = Counter(a.author for a in self.articles())
+        result = [author for author, c in counts.items() if c > 2]
+        return result if result else None
+
+    @classmethod
+    def top_publisher(cls):
+        if not Article.all:
+            return None
+        from collections import Counter
+        return Counter(a.magazine for a in Article.all).most_common(1)[0][0]
